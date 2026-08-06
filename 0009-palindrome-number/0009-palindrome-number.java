c0a8ace691bc64329f5feb186ee98d67a6303478
Gouransh Gupta <gouransh0603@gmail.com>
2026-08-06 22:40:54 +0530

    Time: 6 ms (21.9%), Space: 46 MB (33.97%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0){
+        if(x < 0){
             return false;
         }
-
         String str = Integer.toString(x);
+        if(x == 0){
+            return true;
+        }
+        int len = str.length();
         int left = 0;
         int right = str.length() - 1;
-
         while(left < right){
             if(str.charAt(left) != str.charAt(right)){
                 return false;
