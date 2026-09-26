a8c2a80487025ef77c55cb273ec415e83e73a917
Gouransh Gupta <gouransh0603@gmail.com>
2026-09-26 21:27:42 +0530

    Time: 10 ms (25.69%), Space: 42.1 MB (33.67%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,10 +3,10 @@ class Solution {
 
         if(n == 0){
             return 0;
-        }
+        }   
         if(n == 1){
             return 1;
         }
-        return fib(n - 2) + fib(n - 1);
+        return fib(n - 1) + fib(n - 2);
     }
 }
